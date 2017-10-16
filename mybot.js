4ef1cc85920e8fbbbbe5cac9ffefb793add29edc
GeneratedScript <32853237+GeneratedScript@users.noreply.github.com>
2017-10-16 18:47:23 -0400

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -60,7 +60,7 @@ if (message.content.startsWith("ban")){
 if (message.content.startsWith("+=purge ")){
     let myRole = message.guild.roles.get("364164722859442176");
     if(message.member.roles.has(myRole.id)) {
-       let numberofmessages = message.content.substring(6)
+       let numberofmessages = message.content.substring(8)
        let messagecount = parseInt(numberofmessages);
        message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
       }
