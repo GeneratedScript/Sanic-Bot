bae1f221d2b25e0db361530247e0d7d0330cbfd4
GeneratedScript <32853237+GeneratedScript@users.noreply.github.com>
2017-10-19 00:25:14 -0400

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -1,165 +1,313 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require("./config.json");
-
+const prefix = ">"
 client.on('ready', () => {
-    client.channels.get('364081762688696322').send('Sanic has been updated! Say `=updates` to see updates!');
-    client.user.setGame('say help for help.')
+  client.guilds.channel.sendMessage("Sanic has restarted! @everyone")
+  console.log('Ready!');
+  client.user.setGame(` >help`)
 });
 
-client.on("message", (message) => {
-    if (message.content.startsWith("What's this")) {
-      message.channel.send("This is a ROBLOX game which is still in development. Stick around!");
-    } else
-        if (message.content(bad_words)) {
-            message.delete();
-            message.channel.send("😡 "+message.author+"! Language! 😡")
-        } else
+client.on("guildMemberAdd", member => {
+  let guild = member.guild;
+  guild.defaultChannel.sendMessage(`:inbox_tray: The user ${member.user} has joined this server `)
+});
 
-    if (message.content.startsWith("Ping")) {
-        message.channel.send("pong!");
-}else
+client.on("guildMemberRemove", member => {
+  let guild = member.guild;
+  guild.defaultChannel.sendMessage(`:outbox_tray: The user ${member.user} has left the server. `)
+});
 
-    if (message.content.startsWith("What's this?")) {
-        message.channel.send("This is a ROBLOX game which is still in development. Stick around!");
-} else
-if (message.content.startsWith("=mute")){
-    if(message.author.roles.has("364164722859442176")){
-      let muterole = message.channel.roles.find("370396551283212298")
-    let member = message.mentions.members.first();
-      member.addRole(muterole.id);
-        message.react("👍")
-      msg.channel.send("<@"+member.id+"> was muted!");
-      }
-  } else
-      if (message.content.startsWith("=unmute")){
-    if(message.author.roles.has("364164722859442176")){
-      let muterole = message.guild.roles.find("370396551283212298")
-    let member = message.mentions.members.first();
-      member.removeRole(muterole.id);
-      msg.channel.send("<@"+member.id+"> was unmuted!");
-      }
-  } else
-if (message.content.startsWith("kick")){
-    let myRole = message.guild.roles.get("364164722859442176");
-    if(message.member.roles.has(myRole.id)) {
+client.on("guildCreate", guild => {
+  console.log(`New guild added : ${guild.name}, owned by ${guild.owner.user.username}`)
+});
 
-        let member = message.mentions.members.first();
-        member.kick();
-        message.channel.send("User has been kicked.")
-        message.react("👍")
-      } else {
-        console.log(`Cannot kick. Reason; Insufficient permissions.`);
-        message.channel.send("Cannot kick. Reason; Insufficient permissions.");
-      }
-}else
-if (message.content.startsWith("dm ")){
-    let myRole = message.guild.roles.get("364164722859442176");
-    if(message.member.roles.has(myRole.id)) {
-         message.delete();
-        let member = message.mentions.members.first();
-        member.sendMessage(message.content.substring(3));
-      } else {
-        console.log(`Cannot kick. Reason; Insufficient permissions.`);
-        message.channel.send("Cannot DM. Reason; Insufficient permissions.");
-      }
-}else
-    if (message.content.startsWith("Hi")){
-    var Messages = ['Hello!', 'Hi!', 'ugh. Hi.', 'Henlo']
-var rand = Messages [Math.floor(Math.random()*Messages.length)]
-message.channel.send(rand)
-    }
-    else
-        if (message.content.startsWith("-8ball ")){
-    var Messages = ['It is certain' ,'It is decidedly so' ,'Without a doubt' ,'Yes definitely' ,'You may rely on it' ,'As I see it, yes' ,'Most likely' ,'Outlook good' ,'Yes' ,'Signs point to yes' ,'Reply hazy try again' ,'Ask again later' ,'Better not tell you now' ,'Cannot predict now' ,'Concentrate and ask again', 'Dont count on it' ,'My reply is no' ,'My sources say no' ,'Outlook not so good' ,'Very doubtful']
-var rand = Messages [Math.floor(Math.random()*Messages.length)]
-message.channel.send("🎱 | "+message.author+", I say: **"+rand+"** to: **"+message.content.substring(7)+"**.")
-    }
-    else
-if (message.content.startsWith("ban")){
-    let myRole = message.guild.roles.get("364164722859442176");
-    if(message.member.roles.has(myRole.id)) {
-        let member = message.mentions.members.first();
-        member.ban();
-        message.channel.send("Uh oh! "+member.displayName+" was banned!")
-        message.react("👍")
-      } else {
-        console.log(`Cannot ban. Reason; Insufficient permissions.`);
-        message.channel.send("Cannot ban. Reason; Insufficient permissions.");
-      }
-}else
-if (message.content.startsWith("+=purge ")){
-    let myRole = message.guild.roles.get("364164722859442176");
-    if(message.member.roles.has(myRole.id)) {
-       let numberofmessages = message.content.substring(8)
-       let messagecount = parseInt(numberofmessages);
-       message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
-      }
-}else
-if (message.content.startsWith("=updates")){
-    message.channel.send({embed: {
-        color: 3447003,
-        description: "Updates Include; - `-8ball` command | - bug fixes | - say `Hi`"
-      }});
-}else
-if (message.content.startsWith("say ")){
-        message.channel.send(message.content.substring(4));
-         message.delete();
-}else
-if (message.content.startsWith("help")){
-    message.author.sendMessage("Commands; What is this? | help | leave | Ping | say [string]");
-    message.channel.sendMessage("<@"+message.author.id+">, I have sent current commands to your DMs!");
-    let myRole = message.guild.roles.get("364164722859442176");
-    if(message.member.roles.has(myRole.id)) {
-        message.author.sendMessage("Sanic has detected you have sufficient permissions to kick/ban. Commands [MOD ONLY]; kick [@p] | ban [@p] | dm [@p] [string] | +=purge [num]")
-    }
-}else
-if (message.content.startsWith("leave")){
-message.author.kick();
-}
-if (message.content.startsWith("Credits")){
-    message.channel.send({embed: {
-    color: 3447003,
-    author: {
-      name: client.user.username,
-      icon_url: client.user.avatarURL
-    },
-    title: "Credits Embed",
-    description: "Credits can be found below.",
-    fields: [{
-        name: "Admins:",
-        value: "Admins are **OverpoweredMasterMan#3121**, **TatGuyHyperTrix#9558**, **GeneratedScript#3909**, and **marcusi#8794**."
-      },
-      {
-        name: "Bot:",
-        value: "Sanic Bot was made by **GeneratedScript#3909**."
-      },
-      {
-        name: "Discord:",
-        value: "Discord Server was made by **Ice-Sama#6304**."
-      },
-    {
-      name: "Website Creator(s)/Twitter Creator(s):",
-      value: "Website Creator(s) and Twitter Creator(s) are as follow; **Johnny {Ghost Freak}#1072**, **『PALADIN OF THE BLACK LION』#8077 (btw he's thicc af)**"
-    }
-    ],
-    timestamp: new Date(),
-    footer: {
-      icon_url: client.user.avatarURL,
-      text: "Sanic Says: Stay thicc"
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0]
+  command = command.slice(prefix.length)
+  console.log(command);
+
+  let args = message.content.split(" ").slice(1);
+
+
+  if (command === "say") {
+      message.channel.sendMessage(args.join(" "));
+  }
+
+  if (command === "announce") {
+      if (message.member.hasPermission("ADMINISTRATOR")) {
+       let args = message.content.split(" ").slice(1).join(" ");
+    let split = args.split("-");
+    let url = args[2];
+        message.channel.sendMessage("@everyone", {
+          embed: {
+            color: 0xFFFF00,
+            author: {
+              name: message.author.username,
+              icon_url: message.author.avatarURL
+            },
+            title: ":information_source: Announcement",
+            description: split[0],
+            url: split[1],
+            timestamp: new Date(),
+            footer: {
+              icon_url: message.author.avatarURL,
+              text: message.author.username
+            }
+          }
+      });
     }
   }
-});
+
+  if (command === "setgame") {
+    if (message.author.id == "223557159151992832") {
+    var argresult = args.join(' ');
+    if (!argresult) argresult = null;
+    client.user.setGame(argresult);
+    message.reply("It has been set..");
+    } else {
+      message.reply("DONT EVEN TRY IT BOI!");
+    }
+  }
+
+  if (command === "setstatus") {
+    if (message.author.id == "223557159151992832") {
+    var argresult = args.join(' ');
+    client.user.setStatus(argresult);
+    message.reply("It has been set..");
+    } else {
+      message.reply("DONT EVEN TRY IT BOI!");
+    }
+  }
+
+
+
+if (command === "cusannounce") {
+      if (message.member.hasPermission("ADMINISTRATOR")) {
+       let args = message.content.split(" ").slice(1).join(" ");
+    let split = args.split("-");
+    let url = args[2];
+        message.channel.sendMessage("@everyone", {
+          embed: {
+            color: 0xFF0000,
+            author: {
+              name: message.author.username,
+              icon_url: message.author.avatarURL
+            },
+            title: split[0],
+            description: split[1],
+            url: split[2],
+            timestamp: new Date(),
+            footer: {
+              icon_url: message.author.avatarURL,
+              text: message.author.username
+            }
+          }
+      });
+    }
+  }
+
+  
+
+  if (command === "ping") {
+    message.channel.sendMessage("pong!")
+  }
+
+  if (command === "help") {
+    message.author.sendMessage("Announce - admin - Announces whatever you want it to say. Usage is... >announce (your message)\n cusannounce - admin - Same as announcement, but the usage is.. >cusannounce <title>-<description>-<URL(Optional)>\n 8ball - misc - Makes an inference of what you put after 8ball\n roll - misc - Chooses a random number\n ban - admin - Bans a user - Has to have  'mod-log' channel\n kick - admin - Kicks a user - Has to have  'mod-log' channel\n mute - admin - Has to have  'mod-log' channel \n ping - misc - tests the speed of the bot. \n say - misc - repeats after what you put after it.\n help - misc - Obvious..\n lockdown - admin - Locks a specific channel, so people without roles cannot talk in the channel till the lockdown us lifted. Usage is... >lockdown <milliseconds> \n warn - admin - Wans a user. \n mydinfo - Tells **_YOUR_** info. Usage... >mydinfo");
+    message.reply("Check your DMs.")
+  }
+
+
+
+  if (command === "kms") {
+    message.channel.sendMessage(`:knife: I have successfully killed ${message.author}. :knife:`)
+  }
+
+  if (command == "eval") {
+    if (message.author.id !== "223557159151992832") return;
+    try {
+      var code = args.join(" ");
+      var evaled = eval(code);
+
+      if (typeof evaled !== "string")
+        evaled = require("util")
+        .inspect(evaled);
+        message.channel.sendCode("xl", clean(evaled));
+    } catch (err) {
+      message.channel.sendMessage(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
+  }
+
+  if (command === "8ball") {
+   var ball = ['Yes','No doubt about it','Try again','signs point to yes','I say no','No chance','Dont think so'];
+   message.channel.sendMessage(ball[Math.floor(Math.random () * ball.length)]);
+  }
+
+if (command === "roll") {
+  message.channel.sendMessage(Math.floor(Math.random() * 100));
 }
-    
-  });
 
-  client.on("guildMemberAdd", (member) => {
-    client.channels.get('364081762688696322').send("<@"+member.id+">"+' has joined the server!');
-  });
+if (command === "mute") {
+  if (message.member.hasPermission("ADMINISTRATOR")) {
+   let reason = args.slice(3).join(' ');
+  let user = message.mentions.users.first();
+  let modlog = message.guild.channels.find('name', 'mod-log');
+  let muteRole = client.guilds.get(message.guild.id).roles.find('name', 'Muted!');
+  if (!modlog) return message.reply('I cannot find a mod-log channel').catch(console.error);
+  if (!muteRole) return message.reply('I cannot find a mute role').catch(console.error);
+  if (reason.length < 1) return message.reply('You must supply a reason for the mute.').catch(console.error);
+  if (message.mentions.users.size < 1) return message.reply('You must mention someone to mute them.').catch(console.error);
+  message.delete();
+  const embed = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .addField('Action:', 'Un/Mute')
+    .addField('User:', `${user.username}#${user.discriminator} (${user.id})`)
+    .addField('Moderator:', `${message.author.username}#${message.author.discriminator}`)
+    .addField('Reason', reason);
 
-  client.on("guildMemberLeave", (member) => {
-client.channels.get('364081762688696322').send(member.displayName+' has left the server!')
+  if (!message.guild.member(client.user).hasPermission('MANAGE_ROLES_OR_PERMISSIONS')) return message.reply('I do not have the correct permissions.').catch(console.error);
 
-  });
-  client.login(config.token);
+
+  if (message.guild.member(user).roles.has(muteRole.id)) {
+    message.guild.member(user).removeRole(muteRole).then(() => {
+      client.channels.get(modlog.id).sendEmbed(embed).catch(console.error);
+    });
+  } else {
+    message.guild.member(user).addRole(muteRole).then(() => {
+      client.channels.get(modlog.id).sendEmbed(embed).catch(console.error);
+    });
+
+
+  }
+  }
+};
+
+if (command === "warn") {
+  if (message.member.hasPermission("ADMINISTRATOR")) {
+   let reason = args.slice(1).join(' ');
+  let user = message.mentions.users.first();
+  let modlog = message.guild.channels.find('name', 'mod-log');
+  if (!modlog) return message.reply('I cannot find a mod-log channel');
+  if (reason.length < 1) return message.reply('You must supply a reason for the warning.');
+  if (message.mentions.users.size < 1) return message.reply('You must mention someone to warn them.').catch(console.error);
+  message.delete();
+  const embed = new Discord.RichEmbed()
+  .setColor(0x32CD32)
+  .addField('Action:', 'Warning')
+  .addField('User:', `${user.username}#${user.discriminator}`)
+  .addField('Moderator:', `${message.author.username}#${message.author.discriminator}`)
+  .addField('Reason', reason);
+  return client.channels.get(modlog.id).sendEmbed(embed);
+
+  }
+}
+
+if (command === "ban") {
+  if (message.member.hasPermission("BAN_MEMBERS")) {
+  let reason = args.slice(1).join(' ');
+  let user = message.mentions.users.first();
+  let modlog = message.guild.channels.find('name', 'mod-log');
+  if (!modlog) return message.reply('I cannot find a mod-log channel');
+  if (reason.length < 1) return message.reply('You must supply a reason for the ban.');
+  if (message.mentions.users.size < 1) return message.reply('You must mention someone to ban them.').catch(console.error);
+
+  if (!message.guild.member(user).bannable) return message.reply('I cannot ban that member');
+  message.guild.ban(user, 2);
+  message.delete();
+
+  const embed = new Discord.RichEmbed()
+    .setColor(0xa50d0d)
+    .addField('Action:', 'Ban')
+    .addField('User:', `${user.username}#${user.discriminator} (${user.id})`)
+    .addField('Moderator:', `${message.author.username}#${message.author.discriminator}`)
+    .addField('Reason', reason);
+  return client.channels.get(modlog.id).sendEmbed(embed);
+  }
+}
+
+if (command === "kick") {
+  if (message.member.hasPermission("KICK_MEMBERS")) {
+    let reason = args.slice(1).join(' ');
+  let user = message.mentions.users.first();
+  let modlog = message.guild.channels.find('name', 'mod-log');
+  if (!modlog) return message.reply('I cannot find a mod-log channel');
+  if (reason.length < 1) return message.reply('You must supply a reason for the kick.');
+  if (message.mentions.users.size < 1) return message.reply('You must mention someone to kick them.').catch(console.error);
+message.delete();
+  if (!message.guild.member(user).kickable) return message.reply('I cannot kick that member');
+  message.guild.member(user).kick();
+
+  const embed = new Discord.RichEmbed()
+    .setColor(0xe5ac10)
+    .addField('Action:', 'kick')
+    .addField('User:', `${user.username}#${user.discriminator} (${user.id})`)
+    .addField('Moderator:', `${message.author.username}#${message.author.discriminator}`)
+    .addField('Reason', reason);
+  return client.channels.get(modlog.id).sendEmbed(embed);
+  }
+}
+
+if (command === "lockdown") {
+  if (message.member.hasPermission("ADMINISTRATOR")) {
+   if (!client.lockit) client.lockit = [];
+  let time = args.join(' ');
+  let validUnlocks = ['release', 'unlock'];
+  if (!time) return message.reply('You must set a duration for the lockdown in either hours, minutes or seconds');
+  if (validUnlocks.includes(time)) {
+    message.channel.overwritePermissions(message.guild.id, {
+      SEND_MESSAGES: null
+    }).then(() => {
+      message.channel.sendMessage('Lockdown lifted.');
+      clearTimeout(client.lockit[message.channel.id]);
+      delete client.lockit[message.channel.id];
+    }).catch(error => {
+      console.log(error);
+    });
+  } else {
+    message.channel.overwritePermissions(message.guild.id, {
+      SEND_MESSAGES: false
+    }).then(() => {
+      message.channel.sendMessage(`Channel locked down for ${ms(ms(time), { long:true })}`).then(() => {
+
+        client.lockit[message.channel.id] = setTimeout(() => {
+          message.channel.overwritePermissions(message.guild.id, {
+            SEND_MESSAGES: null
+          }).then(message.channel.sendMessage('Lockdown lifted.')).catch(console.error);
+          delete client.lockit[message.channel.id];
+        }, ms(time));
+
+      }).catch(error => {
+        console.log(error);
+      });
+    });
+  }
+  }
+}
+
+if (command === "mydinfo") {
+
+  let embed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setDescription("This is **_YOUR_** info!")
+  .setColor("0x008B8B")
+  .addField("Username", `${message.author.username}#${message.author.discriminator}`)
+  .addField("ID", message.author.id)
+  .addField("Created on/at", message.author.createdAt)
+
+  message.channel.sendEmbed(embed);
+
+}
+});
+
+function clean(text) {
+  if (typeof (text) === "string")
+    return text.replace(/`/g, "`" + String.fromCharCode(8203))
+    .replace(/@/g, "@" + String.fromCharCode(8203));
+  else
+    return text;
+}
+
+            client.login('MzY5NTA1NzcwODkyNDI3MjY2.DMZl5g.X0L7vxo4bp-yURChLwU-3jCiO58');
