bbfeefb93a033019a398c3b27a1d95e7b76a41d3
GeneratedScript <32853237+GeneratedScript@users.noreply.github.com>
2017-10-19 15:14:40 -0400

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -73,12 +73,6 @@ client.on('message', message => {
       message.reply("DONT EVEN TRY IT BOI!");
     }
   }
-  if (command === "play ") {
-  var voiceChannel = message.member.voiceChannel;
-    voiceChannel.join()
-    var argresult2 = args.join(' ');
-    const dispatcher = connection.playFile(argresult2);
-  }
 
   if (command === "setstatus") {
     if (message.author.id == "223557159151992832") {
