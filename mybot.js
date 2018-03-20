7882d2d375153f3ca086afd85deeca220374479e
GeneratedScript <32853237+GeneratedScript@users.noreply.github.com>
2018-03-20 12:06:16 -0400

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var PastebinAPI = require('pastebin-js'),
+var PastebinAPI = require('pastebin.js'),
 pastebin = new PastebinAPI('3a3f03696725a46033929f7ca868f1f4');
 const prefix = ">"
 client.on('ready', () => {
