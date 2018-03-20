e3f1ce93c8c81448e18fa380e6f99791dcdc9663
GeneratedScript <32853237+GeneratedScript@users.noreply.github.com>
2018-03-20 11:08:10 -0400

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -6,4 +6,30 @@ client.on('ready', () => {
   client.user.setGame(">help", "https://go.twitch.tv/generatedscript");
 });
 
+
+client.on("guildMemberAdd", member => {
+  let guild = member.guild;
+ message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "Member Joined!",
+    url: "https://discord.gg/GwmHMfE",
+    description: ${member.user}+" has joined Cyber!",
+    fields: [{
+        name: "Username",
+        value: ${member.user}
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© GeneratedScript#3909"
+    }
+  }
+});
+});
+
             client.login('NDI1NDUwNTIwMDkwNjQwMzk0.DZHnSA.-mvbi2-1Es7d_B3PgP6-M6X9XCY');
