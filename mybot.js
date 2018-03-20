ed89b1c7767e4bb58626e75ef26744afbabbea64
GeneratedScript <32853237+GeneratedScript@users.noreply.github.com>
2018-03-20 11:45:18 -0400

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var PastebinAPI = require('pastebin-js');
+var pastebin = new PastebinAPI('devkey');
 const prefix = ">"
 client.on('ready', () => {
  
@@ -7,53 +9,92 @@ client.on('ready', () => {
 });
 
 
-client.on("guildMemberAdd", member => {
-  let guild = member.guild;
- guild.defaultChannel.send({embed: {
-    color: 3447003,
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0]
+  command = command.slice(prefix.length)
+
+  let args = message.content.split(" ").slice(1);
+
+
+  if (command === "HWIDS") {
+      var HWIND = args.join(" ");
+     pastebin.getPaste('h9G46QSM')
+     pastebin.then(function (data) {
+    if(data.indexOf(HWIND) >= 0){
+  message.channel.send({embed: {
+    color: 3066993,
     author: {
       name: client.user.username,
       icon_url: client.user.avatarURL
     },
-    title: "Member Joined!",
+    title: "HWID Status for "+HWIND,
     url: "https://discord.gg/GwmHMfE",
-    description: ${member.user}+" has joined Cyber!",
+    description: "Status can be found below.",
     fields: [{
-        name: "Username",
-        value: ${member.user}
+        name: "Status",
+        value: HWIND+" is **Whitelisted**!"
       }
     ],
     timestamp: new Date(),
     footer: {
       icon_url: client.user.avatarURL,
-      text: "© GeneratedScript#3909"
+      text: "© GeneratedScript"
     }
   }
 });
-});
-client.on("guildMemberRemove", member => {
-  let guild = member.guild;
-  guild.defaultChannel.send({embed: {
-    color: 3447003,
+} 
+      else {
+ message.channel.send({embed: {
+    color: 15158332,
     author: {
       name: client.user.username,
       icon_url: client.user.avatarURL
     },
-    title: "Member Joined!",
+    title: "HWID Status for "+HWIND,
     url: "https://discord.gg/GwmHMfE",
-    description: ${member.user}+" just left.",
+    description: "Status can be found below.",
     fields: [{
-        name: "Username",
-        value: ${member.user}
+        name: "Status",
+        value: HWIND+" is **NOT WHITELISTED**!"
       }
     ],
     timestamp: new Date(),
     footer: {
       icon_url: client.user.avatarURL,
-      text: "© GeneratedScript#3909"
+      text: "© GeneratedScript"
     }
   }
 });
+}
+
+     })
+  .fail(function (err) {
+    // Something went wrong
+    message.channel.send({embed: {
+    color: 15158332,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "Something went wrong!",
+    url: "https://discord.gg/GwmHMfE",
+    description: "Status can be found below.",
+    fields: [{
+        name: "Error",
+        value: err
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© GeneratedScript"
+    }
+  }
 });
+  })
+  }
 
             client.login('NDI1NDUwNTIwMDkwNjQwMzk0.DZHnSA.-mvbi2-1Es7d_B3PgP6-M6X9XCY');
